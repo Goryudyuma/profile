063b5d3843909f746368a82646044e4efa3e4b13
Goryudyuma <umaretekyoumade@gmail.com>
2016-08-02 04:30:58 +0900

    add menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { AppBar, Drawer, MenuItem } from 'material-ui';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import * as Colors from 'material-ui/styles/colors';
 import './App.css';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+class Menu extends Component {
+
+	componentWillMount = () => {
+		this.setState({oepn: false});
+	}
+
+	handleToggle = () => this.setState({open: !this.state.open});
+
+	handleClose = () => this.setState({open: false});
+
+	render() {
+		return (
+			<div>
+				<Title menu={this.handleToggle}/>
+				<Drawer
+					docked={false}
+					width={200}
+					open={this.state.open}
+					onRequestChange={(open) => this.setState({open})}
+				>
+					<MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+					<MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+				</Drawer>
+			</div>
+		);
+	}
+}
+
+class Title extends Component {
+	render = () => (
+		<AppBar
+			title="Goryudyuma"
+			style={{backgroundColor:Colors.blue700}}
+			iconClassNameRight="muidocs-icon-navigation-expand-more"
+			onLeftIconButtonTouchTap={this.props.menu}
+			onTitleTouchTap={this.props.menu}
+		/>
+	);
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<MuiThemeProvider>
+					<Menu/>
+				</MuiThemeProvider>
+			</div>
+		);
+	}
 }
 
 export default App;
